7cc569d6187a8702e64e49a152d5e12561a3c381
javifdez <javifdez@air-de-javi>
2024-04-02 21:39:28 +0200

    original

---
+++ PruebaGitHub/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author javifdez
+ *
+ */
+module PruebaGitHub {
+}
---
+++ PruebaGitHub/src/pruebaGitHub/Prueba_GitHub.java
@@ -0,0 +1,10 @@
+package pruebaGitHub;
+
+public class Prueba_GitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
